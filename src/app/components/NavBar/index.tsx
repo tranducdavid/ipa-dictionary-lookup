800ea1be25c38fd0bd80fa5808aca56bc0c47373
David Tran Duc <davidtranduc8@gmail.com>
2022-03-18 08:07:22 +0100

    Add GitHub link

--- src/app/components/NavBar/index.tsx
+++ src/app/components/NavBar/index.tsx
@@ -1,27 +1,62 @@
-import {
-  Box,
-  Container,
-  makeStyles,
-  styled,
-  Typography,
-  useTheme,
-} from '@mui/material';
+import { Box, Container, Link, styled, Typography } from '@mui/material';
+import { ReactComponent as GithubIcon } from './assets/github-icon.svg';
 import React from 'react';
 
 export function NavBar() {
   return (
     <NavBarBox>
-      <Container>
-        <Typography variant="h6">IPA Dictionary Lookup</Typography>
-      </Container>
+      <NavBarContainer>
+        <Typography variant="h6" fontWeight="bold">
+          IPA Dictionary Lookup
+        </Typography>
+        <GitHubLink
+          href="https://github.com/tranducdavid/ipa-dictionary-lookup"
+          target="_blank"
+          title="Github Page"
+          rel="noopener noreferrer"
+        >
+          <StyledGithubIcon />
+          Github
+        </GitHubLink>
+      </NavBarContainer>
     </NavBarBox>
   );
 }
 
+const StyledGithubIcon = styled(GithubIcon)({
+  marginRight: '0.25rem',
+  height: '2rem',
+});
+
+const GitHubLink = styled(Link)(({ theme }) => ({
+  color: theme.palette.primary.contrastText,
+  cursor: 'pointer',
+  textDecoration: 'none',
+  display: 'flex',
+  padding: '0.25rem 1rem',
+  fontSize: '0.875rem',
+  fontWeight: 500,
+  alignItems: 'center',
+
+  '&:hover': {
+    opacity: 0.8,
+  },
+
+  '&:active': {
+    opacity: 0.4,
+  },
+}));
+
 const NavBarBox = styled(Box)(({ theme }) => ({
   display: 'flex',
   alignItems: 'center',
-  height: theme.spacing(6),
+  height: theme.spacing(8),
   backgroundColor: theme.palette.primary.main,
   color: theme.palette.primary.contrastText,
+}));
+
+const NavBarContainer = styled(Container)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
 }));
